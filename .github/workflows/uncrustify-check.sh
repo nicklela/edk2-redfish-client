bc3d2dd68c11f2075f1562af31a315a36d48d0d4
Nickle Wang <nicklew@nvidia.com>
2023-06-07 23:11:42 +0800

    testt

---
+++ .github/workflows/uncrustify-check.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+
+if [ $# -ne 3 ]
+then
+    echo "usage: $0 [PATH to CONFIG FILE] [PATH to REPO.] [NO. of COMMITS]"
+    exit 1
+fi
+
+CONFIG_FILE="$1"
+REPO_PATH="$2"
+NO_COMMITS="$3"
+
+if [ ! -e "$CONFIG_FILE" ]
+then
+    echo "$CONFIG_FILE does not exist"
+    exit 1
+fi
+
+if [ ! -e "$REPO_PATH" ]
+then
+    echo "$REPO_PATH does not exist"
+    exit 1
+fi
+
+cd "$REPO_PATH"
+
+CHANGED_FILES=$(gitdiff --name-only HEAD~$NO_COMMITS)
+for file in $CHANGED_FILES
+do
+  echo "Uncrustify check file: $file"
+  uncrustify -c $CONFIG_FILE -f $file --check
+  echo "res: $?"
+done
+
+exit 0
